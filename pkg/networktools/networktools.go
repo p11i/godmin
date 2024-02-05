216ace42fdeefdf8a4eda436c4960d3392c172ae
p11i <pouyahosseyni7@gmail.com>
2024-02-05 20:09:47 +0330

    Implement CheckConnectivity function in networktools package

---
+++ pkg/networktools/networktools.go
@@ -0,0 +1,9 @@
+package networktools
+
+import "fmt"
+
+// CheckConnectivity checks network connectivity
+func CheckConnectivity() {
+	fmt.Println("Checking network connectivity...")
+	// Implementation here
+}
